4029dc91072837c7dfcd43a51c183493166aba66
michal-lorenc-mobica <130469682+michal-lorenc-mobica@users.noreply.github.com>
2023-05-21 15:20:46 +0200

    Support data in a more convenient way

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"image/color"
 	"math/rand"
+	"os"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -12,7 +15,41 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
+var myQuestions []string
+var myAnswers []string
+
+func loadData(fullPath string) error {
+
+	file, err := os.Open(fullPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	fileScanner := bufio.NewScanner(file)
+
+	fileScanner.Split(bufio.ScanLines)
+	// mechanism to separate questions from answers - used convenction:
+	// is a single line for question (even line number) and single line for answer (odd line number)
+	separator := 0
+	for fileScanner.Scan() {
+		if separator%2 == 0 {
+			myQuestions = append(myQuestions, fileScanner.Text())
+		} else {
+			myAnswers = append(myAnswers, fileScanner.Text())
+		}
+		separator++
+	}
+	return nil
+}
+
 func main() {
+
+	err := loadData("./data.txt")
+	if err != nil {
+		fmt.Printf(err.Error())
+		return
+	}
 
 	myApp := app.New()
 	myWindow := myApp.NewWindow("InterviewHelper")
@@ -40,14 +77,14 @@ func main() {
 		// clean answer field
 		answerText.SetText("")
 
-		index = rand.Intn(len(questions))
-		questionText.SetText(questions[index])
+		index = rand.Intn(len(myQuestions))
+		questionText.SetText(myQuestions[index])
 		questionGenerated = true
 	})
 
 	buttonAnswer := widget.NewButton("Show answer", func() {
 		if questionGenerated {
-			answerText.SetText(answers[index])
+			answerText.SetText(myAnswers[index])
 		} else {
 			answerText.SetText("Please generate question first")
 		}
